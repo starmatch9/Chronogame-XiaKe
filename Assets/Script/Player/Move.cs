700d480f232d708ca827cb7e7b1a66cf35f734d7
starmatch9 <18534538017@163.com>
2025-11-08 14:07:55 +0800

    加入角色移动

---
+++ Assets/Script/Player/Move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [Header("当前属性")]
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //获取输入
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        //float xRaw = Input.GetAxisRaw("Horizontal");
+        //float yRaw = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(x, y);
+        dir.Normalize();
+        //移动
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+}
